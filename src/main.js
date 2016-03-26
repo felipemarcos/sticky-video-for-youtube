3effc7d605dde5002094b02849d58a863d5f03e0
Felipe Marcos Souza dos Santos <FelipeMarcos@MacBook-Pro.local>
2016-03-26 00:26:54 -0300

    ES6

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-/*!
- * Created by Felipe Marcos. 03/19/2016
- */
-
-(function() {
-  var Video = function(options) {
+class Video {
+  constructor() {
     this.options = {
       observe: {
         childList: true,
@@ -13,83 +9,75 @@
 
     this._reload();
 
-    // Should run only once.
     this._controls();
     this._watchPlayerChanges();
-  };
+  }
 
-  // Sticky
-  Video.prototype.stick = function() {
+  stick() {
     this.player.classList.add('player-sticky');
 
     if (this.sidebar) {
-      var sidebarPosition = this.sidebar.getBoundingClientRect();
+      const sidebarPosition = this.sidebar.getBoundingClientRect();
       this.playerApi.style.right = document.body.clientWidth - sidebarPosition.right + 'px';
       this.playerApi.style.width = sidebarPosition.width + 'px';
     }
-  };
+  }
 
-  Video.prototype.unstick = function() {
+  unstick() {
     this.player.classList.remove('player-sticky');
     this.playerApi.style.removeProperty('width');
     this.playerApi.style.removeProperty('right');
-  };
+  }
 
-  Video.prototype.handleScroll = function() {
+  handleScroll() {
     if (window.scrollY > this.scrollHeight && window.innerWidth > 640) {
       this.stick();
     } else {
       this.unstick();
     }
-  };
+  }
 
   // Utils
-  Video.prototype.throttle = function (callback, limit) {
-    var wait = false;
-    return function () {
+  throttle(callback, limit) {
+    let wait = false;
+
+    return () => {
       if (!wait) {
         callback.call();
         wait = true;
-        setTimeout(function () {
+
+        setTimeout(() => {
           wait = false;
         }, limit);
       }
     }
   };
 
-  Video.prototype.isVideoPage = function() {
-    if (location.pathname === '/watch') {
-      return true;
-    }
-
-    return false;
+  isVideoPage() {
+    return location.pathname === '/watch';
   };
 
-  // Methods
-
-  Video.prototype._controls = function() {
-    var button = document.createElement('button');
+  _controls() {
+    const button = document.createElement('button');
     button.classList.add('player-sticky-controls');
-    button.innerHTML = '<svg version="1.1" id="XMLID_1_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 988 982" enable-background="new 0 0 988 982" xml:space="preserve"> <path d="M496,10C232,10,16,226,16,490s216,480,480,480s480-216,480-480S760,10,496,10z M736,670l-60,60L496,550L316,730l-60-60 l180-180L256,310l60-60l180,180l180-180l60,60L556,490L736,670z"/></svg>';
+    button.innerHTML = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 988 982" enable-background="new 0 0 988 982" xml:space="preserve"> <path d="M496,10C232,10,16,226,16,490s216,480,480,480s480-216,480-480S760,10,496,10z M736,670l-60,60L496,550L316,730l-60-60 l180-180L256,310l60-60l180,180l180-180l60,60L556,490L736,670z"/></svg>';
 
     if (this.playerApi) {
       this.playerApi.appendChild(button);
     }
 
     button.addEventListener('click', this._destroy.bind(this));
-    button.addEventListener('touchstart', this._destroy.bind(this));
   };
 
-  Video.prototype._watchPlayerChanges = function() {
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  _watchPlayerChanges() {
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
     this.observer = new MutationObserver( this._reload.bind(this) );
 
     this.observer.observe(document.querySelector('#player'), this.options.observe);
   };
 
-  // Runs everytime there is a page change
-  Video.prototype._reload = function() {
-    // If this isn't the video page, stop right there.
+  // Runs everytime the player changes
+  _reload() {
     if (!this.isVideoPage()) {
       return;
     }
@@ -99,8 +87,7 @@
     this._init();
   }
 
-  // Set elements
-  Video.prototype._setPlayer = function() {
+  _setPlayer() {
     this.playerApi = document.querySelector('#player-api');
     this.player = document.querySelector('#player');
     this.sidebar = document.querySelector('#watch7-sidebar-contents');
@@ -108,14 +95,14 @@
     this.scrollHeight = this.playerApi.clientHeight;
   };
 
-  Video.prototype._removeListeners = function() {
+  _removeListeners() {
     if (this.listener) {
       window.removeEventListener('scroll', this.listener);
       window.removeEventListener('resize', this.listener);
     }
   };
 
-  Video.prototype._listeners = function() {
+  _listeners() {
     this.listener = this.throttle(this.handleScroll.bind(this), 10);
 
     window.addEventListener('scroll', this.listener);
@@ -124,15 +111,15 @@
     this.handleScroll();
   };
 
-  Video.prototype._destroy = function() {
+  _destroy() {
     this.observer.disconnect();
     this._removeListeners();
     this.unstick();
   };
 
-  Video.prototype._init = function() {
+  _init() {
     this._listeners();
   };
+}
 
-  new Video();
-} ());
+new Video();
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './src',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
